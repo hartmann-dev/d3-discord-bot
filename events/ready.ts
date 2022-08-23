6fae38635c7510ae45ad59d0f9a34cf6e755746b
Harald Hartmann <harald@hartmann-dev.net>
2022-08-23 13:42:49 +0200

    setActivity in bot status

--- events/ready.ts
+++ events/ready.ts
@@ -1,8 +1,9 @@
-import { Client } from "discord.js";
+import { ActivityType, Client } from "discord.js";
 
 const name = "ready";
 const once = true;
 const execute = (client: Client) => {
+  client.user?.setActivity({ type: ActivityType.Listening, name: "/d3" });
   console.log(`${client.user?.tag} wurde im Dungeon beschworen.`);
 };
 
